7eae4e9ec1fa1630830bd292ad22e70cbef1a684
Bharat Dwarkani <bharatdwarkanim@syncfusion.com>
2019-05-16 21:47:36 +0530

    Updated startup url

--- src/Startup.cs
+++ src/Startup.cs
@@ -53,8 +53,8 @@ namespace api_docs
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                c.RoutePrefix = string.Empty;                
             });
 
             app.UseHttpsRedirection();
